ef39ec772e6d82faeb43560e29838cdbe182fc00
Odomart <odomart69@gmail.com>
2025-03-19 18:58:44 +0100

    counter

--- counter-v1/programs/counter-v1/src/lib.rs
+++ counter-v1/programs/counter-v1/src/lib.rs
@@ -10,15 +10,22 @@ pub mod counter {
     use super::*;
 
     pub fn initialize_counter(ctx: Context<InitializeCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        counter.authority = ctx.accounts.signer.key();
+
         Ok(())
     }
 }
+
+const ANCHOR_DISCRIMINATOR: usize = 8;
 
 // Context
 #[derive(Accounts)]
 pub struct InitializeCounter<'info> {
     // counter à créer
-    #[account(init, payer = signer, space = 8 + 8)] // 8 pour anchor discrimanteur et 8 pour le u64
+    #[account(init, payer = signer, space = ANCHOR_DISCRIMINATOR + Counter::INIT_SPACE, seeds = [b"counter", signer.key().as_ref()], bump)]
+    // 8 pour anchor discrimanteur et 8 pour le u64
     pub counter: Account<'info, Counter>,
     // signer
     #[account(mut)]
@@ -29,6 +36,8 @@ pub struct InitializeCounter<'info> {
 
 // Accounts
 #[account]
+#[derive(InitSpace)]
 pub struct Counter {
     count: u64,
+    authority: Pubkey,
 }
