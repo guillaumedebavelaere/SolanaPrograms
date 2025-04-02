a3a75c98e4574aa1dbb2312228113df3233b5d02
Odomart <odomart69@gmail.com>
2025-04-02 19:24:24 +0200

    feat: unit test vault

--- vault/tests/vault.ts
+++ vault/tests/vault.ts
@@ -1,16 +1,78 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Vault } from "../target/types/vault";
+import { SystemProgram, Keypair } from "@solana/web3.js";
+import { BN } from "bn.js";
 
 describe("vault", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.Vault as Program<Vault>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  const adminKeypair = Keypair.generate();
+
+  before(async () => {
+    const airdropSig = await provider.connection.requestAirdrop(
+      adminKeypair.publicKey,
+      1 * anchor.web3.LAMPORTS_PER_SOL
+    );
+    await provider.connection.confirmTransaction(airdropSig);
+  });
+
+  it.skip("Vault creation", async () => {
+    const [vault, _bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("vault")],
+      program.programId
+    )
+
+    const tx = await program.methods
+    .createVault()
+    .accounts({
+      signer: provider.wallet.publicKey,
+      vault: vault,
+      systemProgram: SystemProgram.programId
+    })
+    .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Vault deposit", async () => {
+    const [vault, _bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("vault")],
+      program.programId
+    )
+
+    const tx = await program.methods
+    .deposit(new BN(3500000000))
+    .accounts({
+      signer: provider.wallet.publicKey,
+      vault: vault,
+      systemProgram: SystemProgram.programId
+    })
+    .rpc();
+
+    //let vault_after = program.account.vault.fetch(vault);
+    //vault_after.amount > 
+
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Vault withdraw", async () => {
+    const [vault, _bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("vault")],
+      program.programId
+    )
+
+    const tx = await program.methods
+    .withdraw(new BN(100000000))
+    .accounts({
+      signer: provider.wallet.publicKey,
+      vault: vault,
+      admin: adminKeypair.publicKey,
+      systemProgram: SystemProgram.programId
+    })
+    .rpc();
+
     console.log("Your transaction signature", tx);
   });
 });
